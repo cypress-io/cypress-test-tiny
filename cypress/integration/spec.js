acce95ef7a65a625af590cd46204d32fd5aafcef
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-28 14:38:56 -0400

    add cy.exec test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -7,4 +7,32 @@ describe('page', () => {
   it('has h2', () => {
     cy.contains('h2', 'test')
   })
+
+  it('works with exec', () => {
+    cy.exec('echo foo')
+      .its('stdout')
+      .should('eq', 'foo')
+
+    // assuming the folder same as project name
+    cy.exec('pwd')
+      .its('stdout')
+      .should('include', 'foo-page')
+
+    cy.exec('ls')
+
+    cy.exec('node --version')
+      .its('stdout')
+      .should('match', /^v/)
+
+    cy.exec('npm --version')
+      .its('stdout')
+      .should('match', /^\d+\.\d+\.\d+/)
+
+    cy.exec('echo $TERM')
+    cy.exec('echo $SHELL')
+
+    cy.exec('npm run echo -- foo-bar')
+      .its('stdout')
+      .should('match', /foo-bar$/)
+  })
 })
