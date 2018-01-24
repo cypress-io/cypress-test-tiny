94306116928a459f59198f5179b345d34742ad09
Jacob Rothstein <hi@jbr.me>
2018-01-24 11:40:01 -0800

    cypress defaults NODE_ENV to string “undefined”

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,15 @@
-describe('page', () => {
-  it('works', () => {})
-})
+describe("exec environment", () => {
+  it("should not set NODE_ENV to `'undefined'`", () => {
+    cy
+      .exec("echo $NODE_ENV")
+      .its("stdout")
+      .should("eq", "");
+  });
+
+  it("should behave like this for NODE_ENV", () => {
+    cy
+      .exec("echo $RANDOM_UNDEFINED_VAR")
+      .its("stdout")
+      .should("eq", "");
+  });
+});
