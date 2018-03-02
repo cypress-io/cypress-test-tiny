7180bd6b5ea746263c233c71e6a27fb74e05678a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-03-02 08:54:59 -0500

    failing due to obstructive code I think

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,9 @@
-describe('page', () => {
-  it('works', () => {})
+describe('loads github static page', () => {
+  it('works for website that plays hydration tricks', () => {
+    cy.visit('https://glebbahmutov.com')
+  })
+
+  it.skip('works for "normal" static site', () => {
+    cy.visit('https://glebbahmutov.com/draw-cycle')
+  })
 })
