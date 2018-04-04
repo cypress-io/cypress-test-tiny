37074e4aeedc0f3cbdc490e54b0454f7d54b7a5b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-04-04 14:27:25 -0400

    test waiting on XHR

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,15 @@
+/// <reference types="cypress" />
 describe('XHR', () => {
   it('loads server response', () => {
     cy.visit('index.html')
+    cy.contains('#result', 'catchPhrase').should('be.visible')
+  })
+
+  it('waits for server response', () => {
+    cy.server()
+    cy.route(/users/).as('xhr')
+    cy.visit('index.html')
+    cy.wait('@xhr')
     cy.contains('#result', 'catchPhrase').should('be.visible')
   })
 })
