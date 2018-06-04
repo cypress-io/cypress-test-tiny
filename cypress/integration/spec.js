fcd25392a5a0836bbc234143eaf141dee59bc622
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-06-04 09:23:12 -0400

    more tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,15 @@
 describe('page', () => {
   it('works', () => {})
-  it('does not work', () => {
+  it('has named screenshot', () => {
+    cy.screenshot('an image')
+  })
+  it('has unnamed screenshot', () => {
+    cy.screenshot()
+  })
+  it.skip('does not work', () => {
     throw new Error('this test fails on purpose')
+  })
+  it('takes longer than 1 second', () => {
+    cy.wait(1000)
   })
 })
