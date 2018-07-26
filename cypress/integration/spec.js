a3ccb9653c2a4f4a41a4ec60e6280f3e06ffc597
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-07-26 17:24:28 +0200

    wait for 10 seconds in the test to let stuff load

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,5 +1,7 @@
 describe('beta.tenantreport.net', () => {
   it('loads', () => {
     cy.visit('https://beta.tenantreport.net/auth/login')
+    // let any async mp3 load
+    cy.wait(10000)
   })
 })
