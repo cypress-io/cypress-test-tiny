5627b4ffeadeb85e0bf0c90023508397a22cb96a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-12-17 10:28:04 -0500

    test bad args to cy.get

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,16 @@
-describe('page', () => {
-  it('works', () => {})
+it('calls cy get with 2 strings', () => {
+  // TypeError: Cannot read property 'set' of undefined
+  cy.get('.foo', 'bar')
+})
+it('calls cy get with string and number', () => {
+  // TypeError: Cannot read property 'set' of undefined
+  cy.get('.foo', 42)
+})
+it('calls cy get with string and a function', () => {
+  // works (no warnings)
+  cy.get('.foo', () => {})
+})
+it('calls cy get with string and empty object', () => {
+  // works
+  cy.get('.foo', {})
 })
