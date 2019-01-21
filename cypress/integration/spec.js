9566fd84abfe19fa394552919c9b7d0cf97b7f9d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-01-21 16:51:17 -0500

    add simple env test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,7 @@
+/// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {})
+  it('works', () => {
+    expect(Cypress.env('x')).to.equal('foo')
+    expect(Cypress.env('y')).to.equal('bar')
+  })
 })
