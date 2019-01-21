0696ab468bf7ab97f33fca371d2fca62d3263147
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-01-21 16:53:17 -0500

    pass numbers

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    expect(Cypress.env('x')).to.equal('foo')
-    expect(Cypress.env('y')).to.equal('bar')
+    expect(Cypress.env('x')).to.equal(1)
+    expect(Cypress.env('y')).to.equal(2)
   })
 })
