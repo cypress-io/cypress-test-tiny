c9b040c25a37c16969762a63c0e8b4b45db04044
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-01-21 22:27:00 -0500

    try passing object from CLI as json

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,7 +1,6 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    expect(Cypress.env('x')).to.equal(1)
-    expect(Cypress.env('y')).to.equal(2)
+    expect(Cypress.env('obj')).to.deep.equal({foo: 'bar'})
   })
 })
