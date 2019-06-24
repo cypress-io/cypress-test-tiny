19500b3133fb9be63d5b7acb521eab05c4e8fa3e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-24 09:03:57 -0400

    add example failing

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,8 @@
-describe('page', () => {
-  it('works', () => {})
+/// <reference types="cypress" />
+it('catches correctly exception', () => {
+  cy.visit('index.html', {
+    onBeforeLoad (win) {
+      win.navigator.language = 'JavaScript'
+    }
+  })
 })
