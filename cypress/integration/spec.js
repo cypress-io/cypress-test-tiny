a3474b293aa868113cef19b42bc3d6590d1a4cfe
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-09-07 07:57:01 -0400

    use docs with paths test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,10 +1,10 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('/')
+    cy.visit('https://docs.cypress.io/guides/overview/why-cypress.html')
     cy.log('setting href to bar')
     cy.window().then(w => {
-      w.location.href = 'bar'
+      w.location.href = 'writing-and-organizing-tests.html'
     })
   })
 })
