ffad2711d0331fce836bb9ad257fc93c5cab69f0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-09-16 11:29:23 -0400

    example failure

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,25 @@
-describe('page', () => {
-  it('works', () => {})
+/// <reference types="cypress" />
+it('resolves', () => {
+  cy.visit('index.html')
+  // should wait for the promise returned from
+  // the "window.app.resolves()" call to resolve
+  cy.window()
+    .its('app')
+    .invoke('resolves')
+})
+it('rejects', () => {
+  cy.visit('index.html')
+  // should wait for the promise returned from
+  // the "window.app.resolves()" call to reject and fail the test
+  cy.window()
+    .its('app')
+    .invoke('rejects')
+})
+it('rejects with undefined', () => {
+  cy.visit('index.html')
+  // should wait for the promise returned from
+  // the "window.app.resolves()" call to reject and fail the test
+  cy.window()
+    .its('app')
+    .invoke('rejectsWithUndefined')
 })
