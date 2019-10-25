69cd2d00438faf88c55829e5cf10c365a226c604
Damir Vandic <info@dvic.io>
2019-10-25 20:55:36 +0200

    Add test for 3.5 broken input date support

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,9 @@
-describe('page', () => {
-  it('works', () => {})
+describe('typing into date field', () => {
+  it('works', () => {
+    cy.visit("index.html")
+    cy.get("#mydate").type("1995-12-04")
+    cy.window().then(window => {
+      expect(window.receivedInput).to.be.true
+    })
+  })
 })
