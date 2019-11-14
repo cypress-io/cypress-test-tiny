469a7c7987e0ea485d913b4f4262bba84bda9704
Luke Page <lukp@saxobank.com>
2019-11-14 20:13:22 +0100

    Bug for typing 10 into a autofocussing textbox with 1 in it

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,7 @@
 describe('page', () => {
-  it('works', () => {})
+  it('works', () => {
+	  cy.visit(`cypress/fixtures/test.html`);
+	  cy.get('input').type('10');
+	  cy.get('input').should('have.value', 10);
+  })
 })
