4dcef2da4c50d8724f5b718fd43632440f4476a6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-11-15 12:00:30 -0500

    invalid config returned from plugins file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,4 +14,8 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  return {
+    viewportWidth: 'foo',
+    baseUrl: 123
+  }
 }
