1966c415140287811640b78838d17c9f7d7c1e98
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-12-10 17:16:03 -0500

    add failing test that wraps a large array

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,7 @@
-describe('page', () => {
-  it('works', () => {})
+/// <reference types="cypress" />
+it('works', () => {
+  const arr = new Uint8ClampedArray(2560000)
+  cy.wrap(arr)
+  // uncomment to have test hanging trying to inspect the wrapped value in Chai
+  // .should('have.length', 2)
 })
