1f57d0db67bff68f0dc2e008f2ba8e6297704fab
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-12-16 21:31:26 -0500

    failing test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,32 @@
-describe('page', () => {
-  it('works', () => {})
+/// <reference types="cypress" />
+it('one should callback (works)', () => {
+  cy.wrap(null)
+    .should(() => {
+      console.log('in should()')
+      expect(true).to.be.true
+      expect(true).to.be.true
+    })
+})
+
+it('two should callbacks with 1 expect (works)', () => {
+  cy.wrap(null)
+    .should(() => {
+      console.log('in should()')
+      expect(true).to.be.true
+    })
+    .and(() => {
+      console.log('in and ()')
+    })
+})
+
+it('two should callbacks (crashes and burns)', () => {
+  cy.wrap(null)
+    .should(() => {
+      console.log('in should()')
+      expect(true).to.be.true
+      expect(true).to.be.true
+    })
+    .and(() => {
+      console.log('in and ()')
+    })
 })
