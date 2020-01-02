7f0a127d292f45d57157267615b10c17b6883a32
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-01-02 13:21:26 -0500

    assertion after test finish

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,13 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+it('confirms', () => {
+  cy.visit('index.html')
+
+  cy.on('window:confirm', (text) => {
+    // make the assertion fail on purpose
+    // but the test does NOT wait for this assertion to pass
+    // By the time the assertion fails, the test has passed
+    expect(false).to.be.true
   })
+
+  cy.get('#click').click()
 })
