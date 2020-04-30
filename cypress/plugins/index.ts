2d08934929ff781ff6c751c09b4e49e5e09e56c1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-30 17:22:42 -0400

    test alias ts paths

---
+++ cypress/plugins/index.ts
@@ -0,0 +1,3 @@
+import { forward } from '@apps/commons-e2e'
+console.log('forward is', forward)
+export default function () {}
--- src/commons-e2e.js
+++ src/commons-e2e.js
@@ -1,3 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.forward = true;
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var forward;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [],
+        execute: function () {
+            exports_1("forward", forward = true);
+        }
+    };
+});
