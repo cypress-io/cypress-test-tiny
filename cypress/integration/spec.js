08172373ff30d0e78544cc240c0a88155ecf035f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-05 12:17:54 -0400

    show separate bundles

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,13 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+import { Username } from '../support/globals'
+
+describe('globals', () => {
+  it('shows username', () => {
+    console.log('in spec username is "%s"', Username.name)
+    console.log('now changed it to "foo"')
+    Username.name = 'foo'
+    console.log('in spec username is "%s"', Username.name)
+
+    cy.printName()
   })
 })
