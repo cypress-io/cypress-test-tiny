818b5b02943714fee4cdef33fefaf6fba095e234
Martyn Chamberlin <martynchamberlin@icloud.com>
2020-07-28 16:37:51 -0500

    Add a failing test for iFrames

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,24 @@
+Cypress.Commands.add('getFrame', frame => {
+  // get the iframe > document > body
+  // and retry until the body element is not empty
+  return cy
+  .get(`[data-cse="${frame}"] iframe`)
+  .its('0.contentDocument.body').should('not.be.empty')
+  // wraps "body" DOM element to allow
+  // chaining more Cypress commands, like ".find(...)"
+  // https://on.cypress.io/wrap
+  .then(cy.wrap)
+});
+
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
+    cy.visit('/test')
+
+    .wait(1000)
+
+    cy.getFrame('encryptedCardNumber')
+      .find('#encryptedCardNumber')
+      .type(4242424242424242);
+  });
 })
