d35b92504b1c6529b0b60c913c9d45465466b55d
Andrew Zakordonets <biercoff@gmail.com>
2020-08-26 17:29:21 +0200

    reproduce issue with cypress 5.0 webpack compiler

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,10 @@
+import SwaggerPetstore from 'test-petstore-service/api-client/src/api/PetApi'
+
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
+    const pet = new SwaggerPetstore.Pet();
+    cy.log(pet)
     cy.visit('https://example.cypress.io')
   })
 })
