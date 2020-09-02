e426422fb50ec77348a9e27ef5efe8b5802b48f3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-02 17:37:29 -0400

    example getting domain error

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('index.html')
+    cy.contains('h1', 'Utilities').should('exist')
   })
 })
