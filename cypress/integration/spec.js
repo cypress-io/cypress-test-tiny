c1058fccd82bc3c2dfe5f6b2eeae2971c5f6eb1d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-15 15:28:43 -0400

    before hook spec

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,27 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+const clilog = console.log
+
+describe("Suit", () => {
+    before(() => {
+        clilog("Running suit: ");
+    });
+    after(() => {
+        clilog("Completing suit: ");
+    });
+    describe("Test: Some Test", () => {
+        before(() => {
+            clilog("Before Test: ");
+        });
+        after(() => {
+            clilog("After Test: ");
+        });
+        xit( "This is the test to skip", ()=>{
+            //test code
+            clilog("Running Test: ");
+        });
+        it( "This is the sample test", ()=>{
+            //test code
+            clilog("Running Test: ");
+        });
+    });
+});
