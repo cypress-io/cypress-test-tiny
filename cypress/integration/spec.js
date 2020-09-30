6f76b4a0628ebdbdc9efc29f4f96169bd62e1a06
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-30 13:47:59 -0400

    recreate

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,14 @@
 /// <reference types="cypress" />
+Cypress.on('test:after:run', (test) => {
+  if (test.state === 'failed') {
+    // https://github.com/cypress-io/cypress/issues/8701
+    throw new Error('I am throwing')
+  }
+})
+
 describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
+    cy.get('[class^="main-nav-link active"]').should('contain', 'ZZZ')
   })
 })
