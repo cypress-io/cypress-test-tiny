3a735985cddc90986c7d1c8cba0a295285764733
Ismail Metaich <ismail.metaich@gmail.com>
2020-10-04 11:00:13 +0200

    mocha with cypress

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,36 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
+  function waitOneSecond() { // copied from https://docs.cypress.io/api/utilities/promise.html#Usage
+    // return a promise that resolves after 1 second
+    return new Cypress.Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, 1000)
+    });
+  }
+  it('does work in sync', () => {
+    let users;
+    expect(users).to.be.undefined;
+    users = ['a','b','c'];
+    expect(users).not.to.be.undefined;
+  });
+
+  it('does work in async', () => {
+    let users;
+    expect(users).to.be.undefined;
+    waitOneSecond().then(() => {
+      users = ['a','b','c'];
+    })
+    cy.log(users);
+  });
+  
+  it('does not work in async, but should..', () => {
+    let users;
+    expect(users).to.be.undefined;
+    waitOneSecond().then(() => {
+      users = ['a','b','c'];
+    })
+    expect(users).not.to.be.undefined;
+  });
+  
 })
