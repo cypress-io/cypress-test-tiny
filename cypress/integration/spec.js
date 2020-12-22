1669940ef0c087a03963aeff7b337ab83628e6e5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-12-22 14:14:24 -0500

    Cypress.dom.isJquery

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,5 +2,15 @@
 describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
+    cy.contains('Kitchen Sink').then($el => {
+      console.log('is element', Cypress.dom.isElement($el))
+      console.log('is jquery', Cypress.dom.isJquery($el))
+
+      console.log('[0] is element', Cypress.dom.isElement($el[0]))
+      console.log('[0] is jquery', Cypress.dom.isJquery($el[0]))
+
+      console.log('undefined is element', Cypress.dom.isElement())
+      console.log('undefined is jquery', Cypress.dom.isJquery())
+    })
   })
 })
