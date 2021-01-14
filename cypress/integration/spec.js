dae54bf1192e46180050e37430ce9f3cbc56371b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-14 12:26:08 -0500

    set up reproduction

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,24 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+it('Login page shows', () => {
+  cy.visit('app.staging.flkitover.com');
+  cy.clearCookies();
+  cy.location('pathname').should('equal', '/');
+  cy.get('h1').contains('Log In');
+
+  cy.intercept('POST', '/login').as('login');
+  cy.get('[name=email]').type('something@something.com');
+  cy.get('[name=password]').type('Test1234');
+  cy.contains('button', 'Log in').click();
+
+  cy.wait('@login')
+      .its('response.statusCode')
+      .should('eq', 401);
+
+  cy.intercept('POST', '/login').as('login2');
+  cy.get('[name=password]').type('Test1234');
+  cy.contains('button', 'Log in').click();
+
+  cy.wait('@login2')
+      .its('response.statusCode')
+      .should('eq', 401);
+});
