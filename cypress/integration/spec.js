943fb735ec29759cc84b17457ddc5e9a43e0fd0a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-21 10:42:10 -0500

    add test example

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,7 +1,19 @@
 /// <reference types="cypress" />
-it('gets into a loop', () => {
-  cy.wrap('bar')
-    .should(() => {
-      cy.wrap('foo')
-    })
-})
+describe('Check Marketing logging', () => {
+	before(function () {
+		cy.visit('https://www.independer.nl/zorgverzekering/intro.aspx');
+	});
+
+	it('Check button click - intercept req', function () {
+		const requestBody = { "wrong": "request body" };
+
+		cy.intercept('POST', '/api/ue/c', (req) => {
+			expect(JSON.stringify(req.body)).to.include(JSON.stringify(requestBody));
+		}).as('sLIdentifier');
+
+		// cy.getByUe('man', 'input').click({ force: true });
+		cy.get('input[ue="man"]').click({ force: true });
+
+		cy.wait('@sLIdentifier');
+	});
+});
