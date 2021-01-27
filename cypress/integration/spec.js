079583ac6dd1ca9e20f53daeb7e68e4f7372778e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-27 17:28:06 -0500

    update test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,10 +1,7 @@
 /// <reference types="cypress" />
 it('opens the video in frame', () => {
   cy.visit('/')
-  // Scroll to feed
-  cy.get('.cy-grid').scrollIntoView()
-  // Should hover over to the video at index 1
-  cy.get(`.cy-thumbnail-link`).eq(1)
-    .pause()
-    .trigger('click')
+  cy.get(`.cy-thumbnail-link`).first()
+    .should('be.visible')
+    .click()
 })
