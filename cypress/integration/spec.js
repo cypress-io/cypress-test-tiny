c88b2a9e8e34d6de26b4aeffc6574b576c645e49
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-27 13:35:45 -0500

    example adding extra time to the failing tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,5 +2,13 @@
 describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
+    cy.wrap(false, {timeout: 100}).should('be.true')
   })
+})
+
+afterEach(function () {
+  console.log(this.currentTest)
+  if (this.currentTest.state === 'failed') {
+    cy.wait(5000)
+  }
 })
