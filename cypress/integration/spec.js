5a38aaf2048b5c4be79be7abea37ef5275e88735
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-03 21:55:30 -0500

    page and test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,16 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+describe('form', () => {
+  it('should not be submittable', () => {
+    cy.visit('index.html')
+
+    cy.get('#form-validation').within(() => {
+      // note we cannot submit by clicking the button
+      // since some of the input elements on the page
+      // fail the HTML validation
+      cy.get('input[type=submit]').click()
+
+    // but the .submit() happily ignores the checks
+      cy.get('form').submit()
+    })
   })
 })
