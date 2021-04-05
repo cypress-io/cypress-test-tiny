70839734e3cd2e47eda60a46c00442ffb498892f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-05 16:52:00 -0400

    update to show the bug

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,5 +2,15 @@
 it('uses the alias', () => {
   // cy.visit('/')
   cy.visit('index.html')
-  cy.focused().should('have.id', 'fname')
+  cy.focused().should('have.id', 'fname').blur()
+
+  // completely unrelated field
+  cy.contains('Child div').as('child').should('be.visible')
+
+  // re-render the label
+  cy.get('button').click()
+
+  cy.get('@child')
+  // just querying it again works
+  // cy.contains('Child div').as('child').should('be.visible')
 })
