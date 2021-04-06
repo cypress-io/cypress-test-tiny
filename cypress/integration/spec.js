e82ab05ec13f6dad5f32abe3f9571a5f69ac413d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-06 10:25:40 -0400

    reproducible example

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,18 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+
+// load fixture separately
+const basicTest = require('../fixtures/basic-test.json')
+
+// https://github.com/cypress-io/cypress/issues/15736
+describe('nothing', function () {
+  beforeEach(function () {
+    cy.fixture('basic-test').then(function(testdata){
+      this.data = testdata
+    })
+  })
+
+  it('does little', function () {
+    cy.log(this.data)
+    expect(this.data).to.deep.equal(basicTest)
   })
 })
