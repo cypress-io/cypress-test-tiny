0946e50cad6d59b717b868465ae73bdc2fce0e09
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-07-09 09:24:38 -0400

    example

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,26 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  it('works without jumps', () => {
+    cy.visit('index.html')
+    cy.get('button').click()
+  })
+
+  it('works without jumps using CSS resource link', () => {
+    cy.visit('index-with-css-link.html')
+    cy.get('button').click()
+  })
+
+  it('DOM snapshots jump', () => {
+    cy.visit('index-with-css-import.html')
+    cy.get('button').click()
+  })
+
+  it('DOM snapshots jump with CSS resource delay', () => {
+    cy.intercept('reset.css', {
+      fixture: 'reset.css',
+      delay: 500,
+    })
+    cy.visit('index-with-css-import.html')
+    cy.get('button').click()
   })
 })
