d8fcfac38d51ca02393b044676a413a9a6e294dd
tim <tim@glo.id.au>
2021-08-13 10:42:20 +1000

    add a test mount, just incase it helps./... (?)

--- src/TestComponent.test.jsx
+++ src/TestComponent.test.jsx
@@ -1,9 +1,13 @@
+import { mount } from "@cypress/react";
+import { TestComponent } from "./TestComponent";
+
 it("should call window:before:load", () => {
   const beforeLoadStub = cy.stub().as("beforeLoadStub");
   cy.on("window:before:load", (win) => {
     console.log("#######################", win);
     beforeLoadStub();
   });
+  mount(<TestComponent />);
   cy.wrap({ moo: true }).its("moo").should("equal", true);
   cy.get("@beforeLoadStub").should("have.been.called");
 });
