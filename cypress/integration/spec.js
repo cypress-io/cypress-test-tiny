fddb7a3e35e40ba24a466610327b294d3490e056
Marc-Andre Lafortune <github@marc-andre.ca>
2021-12-16 13:14:53 -0500

    Shows `cy.root()` inside a `within` should retry but doesn't

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,42 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+
+// All tests should pass
+
+describe("page", () => {
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io");
+    writeHelloAfter1s();
+  });
+
+  it("`root` inside a `within` should retry but doesn't (bug)", () => {
+    cy.get("body").within(() => {
+      cy.root().should("contain", "Hello");
+    });
+  });
+
+  it("`root` outside any `within` does retry (good)", () => {
+    cy.root().should("contain", "Hello");
+  });
+
+  it("`get` inside a `within` does retry (good)", () => {
+    cy.get("body").within(() => {
+      cy.get(".banner:first").should("contain", "Hello");
+    });
+  });
+
+  it("`root` inside a `within` with a `then` does retry (good)`", () => {
+    cy.get("body").within(() => {
+      cy.root().then(($body) => cy.wrap($body).should("contain", "Hello"));
+    });
+  });
+});
+
+function writeHelloAfter1s() {
+  cy.window().then((w) => {
+    w.eval(`setTimeout(() => {
+        document.querySelector('.banner .container').innerHTML = '<p> Hello, world!</p>'
+      }, 1000)
+      console.log("ok")
+      `);
+  });
+}
