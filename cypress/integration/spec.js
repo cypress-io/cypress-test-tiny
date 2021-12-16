719748882e3008e7a0e9067d8e49d2170c619177
Marc-Andre Lafortune <github@marc-andre.ca>
2021-12-16 13:14:53 -0500

    Show dynamic config fail

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,40 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+
+// All tests should pass
+
+describe("page", () => {
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io");
+    delayedHelloAfter(7);
+  });
+
+  it("`get` retries using the defaultTimeout so this test fails (good)", () => {
+    cy.get("body").should("contain", "Hello");
+  });
+
+  it("`get` respects the `timeout` option so this test passes (good)", () => {
+    cy.get("body", { timeout: 10000 }).should("contain", "Hello");
+  });
+
+  it("`get` does not respect the `defaultCommandTimeout` changed via `config` so this fails (bad)", () => {
+    Cypress.config("defaultCommandTimeout", 10000);
+    cy.get("html").should("contain", "Hello");
+  });
+
+  it("This form should be equivalent to the previous one, but passes (good)", () => {
+    Cypress.config("defaultCommandTimeout", 10000);
+    cy.get("html", {
+      timeout: Cypress.config().defaultCommandTimeout,
+    }).should("contain", "Hello");
+  });
+});
+
+function delayedHelloAfter(seconds) {
+  cy.window().then((w) => {
+    w.eval(`setTimeout(() => {
+        document.querySelector('.banner .container').innerHTML = '<p> Hello, world!</p>'
+      }, ${seconds} * 1000)
+      console.log("ok")
+      `);
+  });
+}
