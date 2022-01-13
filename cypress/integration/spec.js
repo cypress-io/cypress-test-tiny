5f9aacb2f25d4aec9c78498e142c7d66cf762210
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-01-13 16:42:09 -0500

    show crash

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+
+// without the test config object the test correctly is set to pending status
+// but with this test config object you get an exception trying to run it
+/*
+TypeError: fn.apply is not a function
+  at Context.runnable.fn (http://localhost:53035/__cypress/runner/cypress_runner.js:172338:19)
+  at callFn (http://localhost:53035/__cypress/runner/cypress_runner.js:125099:21)
+  at Test.../driver/node_modules/mocha/lib/runnable.js.Runnable.run (http://localhost:53035/__cypress/runner/cypress_runner.js:125086:7)
+*/
+it('works', {viewportWidth: 100})
