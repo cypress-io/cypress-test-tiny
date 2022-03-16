6fd465001f51dab29ae681f9279d2b5ce42676d1
Emily Rohrbough <emilyrohrbough@yahoo.com>
2022-03-16 09:25:51 -0500

    try to reproduce https://github.com/cypress-io/cypress/issues/20638

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,19 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+describe('todo list', () => {
+  it('add long todo', () => {
+    cy.visit('https://example.cypress.io/todo')
+      const newItem = 'Feed the cat'
+      const newItem2 = 'HELLO do I work as expected?!'
+      const randomStringWithCaps = 'ADJSHFUIYRHEFJBKJVBJ'
+  
+      cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
+      cy.get('[data-test=new-todo]').type(`${newItem2}{enter}`)
+      cy.get('[data-test=new-todo]').type(`${randomStringWithCaps}{enter}`)
+      cy.get('[data-test=new-todo]').type(`${randomStringWithCaps}{enter}`)
+  
+      cy.get('.todo-list li')
+      .should('have.length', 6)
+      .last()
+      .should('have.text', randomStringWithCaps)
   })
 })
