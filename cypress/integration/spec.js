e103cf7190ad3ec24619848127f8a25561128133
Nicolas Ramz <nicolas.ramz@adevinta.com>
2022-04-19 15:20:16 +0200

    ADDED: test spec to repro load timeout on redirect

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    // this page (see: https://github.com/warpdesign/cypress-test-redirect-before-onload/blob/gh-pages/index2.html)
+    // attempts to change the page's location before the onload event gets fired.
+    // This makes cypress wait forever for the load event to be fired and eventually
+    // triggers a timeout error
+    cy.visit('/index2.html')
+    // replacing the above line by this one does not trigger the problem:
+    // cy.visit('/')
   })
 })
