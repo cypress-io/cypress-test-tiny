b29679f03be37ea667988b54ca1ba12f63210fc5
Emily Rohrbough <emilyrohrbough@yahoo.com>
2022-07-31 15:59:12 -0500

    double check that this is causing hangs in run mode

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,19 @@
-/// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+const shouldNotExecute = () => {
+  throw new Error('Test Override validation should have failed & it block should not have executed.')
+}
+
+Cypress.on('test:before:run', () => {
+  console.log('test:before:run....throw an error')
+  throw new Error('Test Override validation should have failed & it block should not have executed.')
+})
+
+
+it('does not run', () => {
+  shouldNotExecute()
+})
+
+describe('nested', () => {
+  it('does not run 2', () => {
+    shouldNotExecute()
   })
 })
