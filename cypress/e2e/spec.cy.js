13f03de442439375959a9271316f88bf5efd7a8e
nandkumar ghatge <nandkumar.ghatge@smartbox.com>
2023-09-15 10:04:59 +0100

    added test to reprduce the issue

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,14 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    // run the test in chrome
+    // open browser and check console
+    // it throw error 
+    // Refused to load the script 'https://www.google.com/recaptcha/enterprise.js?render=123&render=explicit' because it violates the following Content Security Policy directive: "script-src 'unsafe-eval'". Note that 'script-src-elem' was not explicitly set, so 'script-src' is used as a fallback.
+    var scr  = document.createElement('script'),
+
+        head = document.head || document.getElementsByTagName('head')[0];
+        head.appendChild(scr)
+        scr.src = 'https://www.google.com/recaptcha/enterprise.js?render=123&render=explicit';
   })
 })
