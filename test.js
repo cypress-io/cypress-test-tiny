f69213a5f5d4f1ea75f168e5c255f37f32eced31
Cacie Prins <cacie@cypress.io>
2024-04-15 12:59:33 -0400

    confg

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+const cypress = require('cypress')
+
+cypress.run({
+  reporter: 'junit',
+  browser: 'chrome',
+  spec: './cypress/e2e/spec.cy.js',
+  config: {
+    baseUrl: 'https://share.vidyard.com',
+    video: true,
+  }
+}).then(results => {
+  console.log('results!')
+}).catch(err => {
+  console.err(err)
+}).finally(() => {
+  process.exit()
+})
