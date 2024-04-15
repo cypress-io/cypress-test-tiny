7c1ebe201ec752bddd8a74869bde3961c1a67a25
Cacie Prins <cacie@cypress.io>
2024-04-15 12:52:35 -0400

    run with module api

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,31 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
+  it('Error: WebSocket connection closed', function () {
+    cy.visit('https://share.vidyard.com/watch/mR7nXEXcdnRMCyYG52bVxj');
+    cy.get('#vidyard-logo')
+      .find('img')
+      .should('be.visible');
+    cy.get('head link[rel*="icon"]')
+      .should('have.attr', 'href', '/favicon.ico');
+    cy.get('#get-vidyard-free-cta')
+      .should('be.visible')
+      .and('have.text', 'Sign Up for Free');
+    cy.get('body')
+      .should('have.css', 'background-color', 'rgb(255, 255, 255)');
+    cy.get('#name-header')
+      .find('h1')
+      .should('be.visible')
+      .and('have.text', 'Paint Squiggles');
+    cy.get('#email-signup-button')
+      .should('be.visible')
+      .and('have.text', 'Sign up with Business Email');
+    const socialLogins = [
+      '#google-signup-button',
+      '#outlook-signup-button',
+    ];
+    socialLogins.forEach((element) => {
+      cy.get(element)
+        .should('be.visible');
+    });
+  });
 })
