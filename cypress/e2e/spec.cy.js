393a2a90aa48262aef01fbde5ce887381d6ca37a
Cacie Prins <cacie@cypress.io>
2024-10-17 11:12:03 -0400

    invoke example

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  it('... ', () => {
+    cy.visit('/cypress/fixtures/input.html')
+    cy.get('#input').invoke('val', 'foo').trigger('blur')
+    cy.get('#input').should('have.value', 'foo')
   })
 })
